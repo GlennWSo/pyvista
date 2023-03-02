c871273f8c75cc78b187601048aca46554fd4336
GlennWSo <sondellglenn@gmail.com>
2023-03-02 09:14:26 +0100

    each defaultSystem

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   inputs.dream2nix.url = "github:nix-community/dream2nix";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = inp:
     inp.dream2nix.lib.makeFlakeOutputs {
-      systemsFromFile = ./nix_systems;
+      systems = inp.flake-utils.lib.defaultSystems;
       config.projectRoot = ./.;
       source = ./.;
       projects = ./projects.toml;
